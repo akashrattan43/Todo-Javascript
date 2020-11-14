b6f22ad8b10e1046a1c2d63ccc7274258d8720e7
akash rattan <akashrattan43@Gmail.com>
2020-11-13 22:33:49 -0800

    styling finished to ul and li

---
+++ app.js
@@ -0,0 +1,36 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event listeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+
+function addTodo(event) {
+  // Prevent form from submitting
+  event.preventDefault();
+  //Todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create Li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  // Check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //Trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //append to list
+  todoList.appendChild(todoDiv);
+  //clear todo input value
+  todoInput.value = "";
+}
